68ee306aa484244352b3eb6acd832fc7784af7e3
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-12-04 17:27:47 +0300

    Working facial recognition

--- facial_emotion_recognition.py
+++ facial_emotion_recognition.py
@@ -10,6 +10,21 @@ while True:
     if not ret:
         break
 
+    # Facial identification and processing
+    face_identifier = cv2.CascadeClassifier(
+        'facial_haarcascade/haarcascade_frontalface_default.xml'
+    )
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_identifier.detectMultiScale(
+        gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+        roi_gray_frame = gray_frame[y:y+h, x:x+w]
+        cropped_image = np.expand_dims(np.expand_dims(
+            cv2.resize(roi_gray_frame, (48, 48)), -1
+        ), 0)
+
     cv2.imshow('FACIAL EMOTION RECOGNITION', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
