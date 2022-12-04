760434c6b67b924a9346200052ee379cda6c3c61
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-12-04 18:01:14 +0300

    FER neural network

--- facial_emotion_recognition_model.py
+++ facial_emotion_recognition_model.py
@@ -26,3 +26,33 @@ validation_generator = validation_data_generator.flow_from_directory(
     color_mode='grayscale',
     class_mode='categorical'
 )
+
+# FER CNN
+facial_emotion_recognition_model = Sequential()
+input_shape = (48, 48, 1)
+
+# Layer 1
+facial_emotion_recognition_model.add(Conv2D(32, kernel_size=(3, 3),
+                                            activation='relu', input_shape=input_shape))
+facial_emotion_recognition_model.add(
+    Convo2D(64, kernel_size=(3, 3), activation='relu'))
+facial_emotion_recognition_model.add(MaxPool2D(pool_size=(2, 2)))
+facial_emotion_recognition_model.add(Dropout(0.25))
+
+# Layer 2
+facial_emotion_recognition_model.add(Conv2D(128, kernel_size=(3, 3),
+                                            activation='relu'))
+facial_emotion_recognition_model.add(MaxPool2D(pool_size=(2, 2)))
+facial_emotion_recognition_model.add(
+    Convo2D(128, kernel_size=(3, 3), activation='relu'))
+facial_emotion_recognition_model.add(MaxPool2D(pool_size=(2, 2)))
+facial_emotion_recognition_model.add(Dropout(0.25))
+
+# Last layer
+facial_emotion_recognition_model.add(Flatten())
+facial_emotion_recognition_model.add(Dense(1024, activation='relu'))
+facial_emotion_recognition_model.add(Dropout(0.5))
+facial_emotion_recognition_model.add(Dense(7, activation='softmax'))
+
+facial_emotion_recognition_model.compile(loss='categorical_crossentropy', optimizer=Adam(
+    learning_rate=0.0001, decay=1e-6), metrics=['accuracy'])
