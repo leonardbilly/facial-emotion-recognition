24b773d895e434ebe88b52a2d1c3264ee4cbc578
LEONARD BILLY SSEKANJAKO <leonardobilly8@gmail.com>
2022-12-05 17:21:57 +0300

    Fixed print errors

--- emotion_correlation_matrices.py
+++ emotion_correlation_matrices.py
@@ -1,5 +1,5 @@
 '''
-Emotion correlation matrices calculator that generates the Pearson's correlation between emotions using emotion feature averages calculated from images grouped under different emotions in the dataset.
+Emotion correlation matrices calculator that generates the Pearson's correlation between emotions using emotion feature averages calculated from images grouped under different emotions in the dataset. Plottable correlations fit into the heatmap
 Dependencies: numpy
 '''
 import numpy as np
@@ -15,17 +15,16 @@ def generate_emotion_correlation_matrices(emotion_features_averages):
         plottable_correlation = []
 
         for other_emotion_name, other_emotion_features_average in emotion_features_averages.items():
-            # {emotion_name} : {other_emotion_name} : {emotion_features_average} : {other_emotion_features_average}
             emotion_correlation_matrix = np.corrcoef([emotion_features_average, 0], [
                                                      other_emotion_features_average, 0])
 
             emotion_correlation_matrices[emotion_name][other_emotion_name] = emotion_correlation_matrix
             plottable_correlation.append(
                 np.linalg.det(emotion_correlation_matrix))
+            print(
+                f'\tFinished calculating {emotion_name}-{other_emotion_name} correlation matrix')
 
         plottable_correlations.append(plottable_correlation)
-        print(
-            f'\tFinished calculating {emotion_name}-{other_emotion_name} correlation matrix')
         print(
             f'Finished calculating {emotion_name} emotion correlation matrices.\n')
     print('Done calculating all emotion correlation matrices')
